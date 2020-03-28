141725643811163959e38fa00292118c82aa60c6
pietelite <pieter2@illinois.edu>
2020-03-28 19:32:57 -0400

    Update version number

--- src/main/java/com/minecraftonline/griefalert/util/Reference.java
+++ src/main/java/com/minecraftonline/griefalert/util/Reference.java
@@ -12,7 +12,7 @@ public abstract class Reference {
 
   public static final String ID = "griefalert";
   public static final String NAME = "GriefAlert";
-  public static final String VERSION = "1.0.4";
+  public static final String VERSION = "1.1.1";
   public static final String DESCRIPTION = "A 'Grief' alerting tool.";
   public static final String[] AUTHORS = {"PietElite"};
   public static final String WEBSITE = "https://www.minecraftonline.com";
